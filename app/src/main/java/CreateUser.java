da2d73c5e2dab9e6ea1906418535774fe9f21fcf
dellthePROgrammer <delpierobatou@gmail.com>
2025-01-11 15:04:08 -0500

    Initial Commit

---
+++ app/src/main/java/CreateUser.java
@@ -0,0 +1,158 @@
+
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import androidx.annotation.RequiresApi;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class CreateUser extends Activity {
+
+    FirebaseDatabase firebase;
+    DatabaseReference userDatabase;
+    private FirebaseAuth auth;
+    private Button create, back;
+
+    private ProgressBar progressBar;
+
+    private EditText createUsernameEDT, passwordEDT, passwordConfirmation, fullNameEDT;
+
+    @SuppressLint("MissingInflatedId")
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_user);
+        firebase = FirebaseDatabase.getInstance();
+        userDatabase = firebase.getReference("/users");
+        auth = FirebaseAuth.getInstance();
+
+        createUsernameEDT = (EditText) findViewById(R.id.usernameEDT);
+        passwordEDT = (EditText) findViewById(R.id.createUserPasswordEDT);
+        passwordConfirmation = (EditText) findViewById(R.id.createUserPasswordConfirmationEDT);
+        fullNameEDT = findViewById(R.id.fullName);
+
+        create = findViewById(R.id.createUserBTN);
+        back = findViewById(R.id.returnToSigninBTN);
+        progressBar = findViewById(R.id.progressBar);
+
+
+        create.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View v) {
+                createAccount();
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent changeScreens = new Intent(CreateUser.this, SignInScreen.class);
+                startActivity(changeScreens);
+            }
+        });
+
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        FirebaseUser currentUser = auth.getCurrentUser();
+        if(currentUser != null){
+            reload();
+        }
+    }
+
+    private void createAccount() {
+        progressBar.setVisibility(View.VISIBLE);
+
+        String email = createUsernameEDT.getText().toString().trim();
+        String fullName = fullNameEDT.getText().toString().trim();
+        String password = passwordEDT.getText().toString();
+        String passwordConf = passwordConfirmation.getText().toString();
+
+        // below line is for checking whether the
+        // edittext fields are empty or not.
+        if (TextUtils.isEmpty(email) && TextUtils.isEmpty(password) && TextUtils.isEmpty(passwordConf)) {
+            // if the text fields are empty
+            // then show the below message.
+            progressBar.setVisibility(View.INVISIBLE);
+            createUsernameEDT.setError("You must enter something!");
+            fullNameEDT.setError("You must enter something!");
+            passwordEDT.setError("You must enter something!");
+            passwordConfirmation.setError("You must enter something!");
+        } else if (!password.equals(passwordConf)) {
+            progressBar.setVisibility(View.INVISIBLE);
+            passwordEDT.setError("Passwords do not match!");
+            passwordConfirmation.setError("Passwords do not match!");
+        }else if(passwordEDT.getText().length() < 8){
+            progressBar.setVisibility(View.INVISIBLE);
+            passwordEDT.setError("Password must be 8 characters long!");
+            passwordConfirmation.setError("Password must be 8 characters long!");
+        }
+        else {
+
+            // [START create_user_with_email]
+            auth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, task -> {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = auth.getCurrentUser();
+                            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder().setDisplayName(fullName).build();
+                            assert user != null;
+                            user.updateProfile(profileUpdates).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()){
+                                        Log.d(TAG, "User Profile updated");
+                                    }
+                                }
+                            });
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            progressBar.setVisibility(View.INVISIBLE);
+                            Toast.makeText(CreateUser.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    });
+            // [END create_user_with_email]
+        }
+    }
+
+    private void reload() {
+        Intent changeScreens = new Intent(CreateUser.this, mainPage.class);
+        startActivity(changeScreens);
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if(user != null) {
+            Intent changeScreens = new Intent(CreateUser.this, mainPage.class);
+            startActivity(changeScreens);
+        }
+        progressBar.setVisibility(View.INVISIBLE);
+    }
+}
---
+++ app/src/main/java/SignInScreen.java
@@ -0,0 +1,149 @@
+
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+public class SignInScreen extends AppCompatActivity {
+
+    FirebaseDatabase firebase;
+    FirebaseAuth auth;
+
+    private Button signin, createAccount, offline;
+    private EditText usernameedt, passwordedt;
+    private String email, password;
+    private ProgressBar progressBar;
+    private FrameLayout frameLayout;
+
+    @SuppressLint("MissingInflatedId")
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+        firebase = FirebaseDatabase.getInstance();
+        auth = FirebaseAuth.getInstance();
+
+        //get rid of action bar when running the app
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        signin = findViewById(R.id.signinBTN);
+        createAccount = findViewById(R.id.createAccountBTN);
+        usernameedt = findViewById(R.id.usernameEDT);
+        passwordedt = findViewById(R.id.passwordEDT);
+        progressBar = findViewById(R.id.progressBar2);
+        frameLayout = findViewById(R.id.frameLayout);
+        offline = findViewById(R.id.offline);
+
+        offline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent changeScreens = new Intent(SignInScreen.this, offlinePage.class);
+                startActivity(changeScreens);
+            }
+        });
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
+        createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //screen will be changed to the CreateUser Screen
+                Intent changeScreens = new Intent(SignInScreen.this, CreateUser.class);
+                startActivity(changeScreens);
+            }
+        });
+
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        FirebaseUser currentUser = auth.getCurrentUser();
+        if(currentUser != null){
+            reload();
+        } else{
+
+        }
+    }
+
+    private void signIn() {
+
+        frameLayout.setVisibility(View.VISIBLE);
+        progressBar.setVisibility(View.VISIBLE);
+
+        email = usernameedt.getText().toString().trim();
+        password = passwordedt.getText().toString();
+        // [START sign_in_with_email]
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = auth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            frameLayout.setVisibility(View.INVISIBLE);
+                            progressBar.setVisibility(View.INVISIBLE);
+                            Toast.makeText(SignInScreen.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    private void reload() {
+        Intent changeScreens = new Intent(SignInScreen.this, mainPage.class);
+        startActivity(changeScreens);
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if(user != null) {
+            Intent changeScreens = new Intent(SignInScreen.this, mainPage.class);
+            startActivity(changeScreens);
+        }
+        frameLayout.setVisibility(View.INVISIBLE);
+        progressBar.setVisibility(View.INVISIBLE);
+    }
+
+}
