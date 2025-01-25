a264b041d448d3f162bb17b3d95a647eb2e9e143
ChristianTalia1 <chr3christian.talia@gmail.com>
2025-01-25 12:59:07 -0500

    various fixes
    various stuff around the app

--- app/src/test/java/com/example/aquaanalyzomatic/SignInScreen.java
+++ app/src/test/java/com/example/aquaanalyzomatic/SignInScreen.java
@@ -6,6 +6,7 @@ import static androidx.constraintlayout.motion.widget.MotionScene.TAG;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -23,11 +24,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class SignInScreen extends AppCompatActivity {
 
@@ -43,7 +40,7 @@ public class SignInScreen extends AppCompatActivity {
     @SuppressLint("MissingInflatedId")
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signin);
+        setContentView(R.layout.activity_sign_in_screen);
         firebase = FirebaseDatabase.getInstance();
         auth = FirebaseAuth.getInstance();
 
