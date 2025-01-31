7c2094eea848621e0790338d5fb6ff86b5d23d8b
ChristianTalia1 <chr3christian.talia@gmail.com>
2025-01-30 20:05:39 -0500

    Fixed errors

--- app/src/main/java/com/example/aquaanalyzomatic/CreateUser.java
+++ app/src/main/java/com/example/aquaanalyzomatic/CreateUser.java
@@ -1,6 +1,6 @@
 package com.example.aquaanalyzomatic;
 
-import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+import static androidx.constraintlayout.motion.widget.MotionScene.TAG;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
@@ -41,9 +41,9 @@ public class CreateUser extends Activity {
     @SuppressLint("MissingInflatedId")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setConte      // Retrieve the data passed from SignInScreen
+        // Retrieve the data passed from SignInScreen
         Intent intent = getIntent();
-        String username = intent.getStringExtra("username");ntView(R.layout.activity_create_user);
+        //String username = intent.getStringExtra("username");ntView(R.layout.activity_create_user);
         firebase = FirebaseDatabase.getInstance();
         userDatabase = firebase.getReference("/users");
         auth = FirebaseAuth.getInstance();
@@ -59,7 +59,7 @@ public class CreateUser extends Activity {
 
 
         create.setOnClickListener(new View.OnClickListener() {
-            @androidx.test.filters.SdkSuppress(minSdkVersion = Build.VERSION_CODES.O)
+            //@androidx.test.filters.SdkSuppress(minSdkVersion = Build.VERSION_CODES.O)
             @Override
             public void onClick(View v) {
                 createAccount();
