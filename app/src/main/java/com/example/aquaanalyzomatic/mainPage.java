afc98f4b1c9c27614349a57d4ac0d5a2dd39245d
Pix_XL <82770059+PIXLwastaken@users.noreply.github.com>
2025-02-08 14:39:32 -0500

    teleop finished, shila can pretty it up later bleh :p

--- app/src/main/java/com/example/aquaanalyzomatic/mainPage.java
+++ app/src/main/java/com/example/aquaanalyzomatic/mainPage.java
@@ -1,10 +1,16 @@
 package com.example.aquaanalyzomatic;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class mainPage extends AppCompatActivity {
+
+    private Button submitBtn, signOutBtn, autonFieldBtn;
+    private EditText matchNum, teamNum, L1Num, L2Num, L3Num, L4Num, netAttemptsNum, netScoredNum, humanPlayerAttemptsNum, humanPlayerScoredNum;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,5 +19,7 @@ public class mainPage extends AppCompatActivity {
         // Retrieve the data passed from SignInScreen
         Intent intent = getIntent();
         String username = intent.getStringExtra("username");
+
+
     }
 }
